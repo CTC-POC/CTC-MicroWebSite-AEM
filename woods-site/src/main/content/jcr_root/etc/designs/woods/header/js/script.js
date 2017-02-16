b50306d164831e84c04401f206fb80520ca64d77
IshitaGandhi <ishita.gandhi@cognizant.com>
2017-02-16 21:15:06 +0530

    HTML Integration Files for all the components including styles

--- woods-site/src/main/content/jcr_root/etc/designs/woods/header/js/script.js
+++ woods-site/src/main/content/jcr_root/etc/designs/woods/header/js/script.js
@@ -1,22 +1,22 @@
-$(document).ready(function(){
-	$(".search__input").on("keyup",function(){
-		if($(this).val() == ""){
-			$(".search__button").attr("disabled", true).css("background-color","#969696");
-		}else{
-		
-		$(".search__button").attr("disabled", false).css("background-color","#64a70b");
-            var serachKeyWord= $(this).val();
-            console.log(serachKeyWord);
-		}
-	
-	})
-	$(window).resize(function(){
-		if ($(this).width() < 1028) {
-		
-			$(".menu_dynamic.dropdown-toggle").removeClass("disabled");
-		}
-		else {
-			$(".menu_dynamic.dropdown-toggle").addClass("disabled");
-		}
-	})
-});
+(function( $ ) {
+  $(function() {
+    $( ".search__input" ).keyup(function() {
+
+        if($(this).val().length <= 1) {
+                $(".search__button").attr("disabled", true).css("background-color", "#969696");
+             	$(".advanced_Search").attr("disabled", true)
+            }
+            else {
+                $(".search__button").attr("disabled", false).css("background-color", "#64a70b");
+                $(".advanced_Search").attr("disabled", false)
+            }
+
+    });
+      $(".search__button, .advanced_Search" ).click(function() {
+	       var search_value = $('.search__input').val();
+			console.log(search_value);
+          window.location ="https://10.226.179.82:9002/ctcstorefront/en/search/?text=woods";
+      });
+
+  });
+})(jQuery);
